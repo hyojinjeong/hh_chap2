cecafc0de96dbf71f1114c4a35b987d952620fc0
hyojinjeong <hyojin.dev@gmail.com>
2024-03-22 01:16:47 +0900

    add : chap2 과제 B

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,14 +1,40 @@
 export function createHooks(callback) {
+  let stateIdx = 0;
+  let stateArr = [];
+  let setStateArr = [];
+
   const useState = (initState) => {
-    return [];
-  };
+    let state;
+    let setState;
+
+    if (stateArr[stateIdx] === undefined) {
+      state = initState;
+    } else {
+      state = stateArr[stateIdx];
+    }
+    if (setStateArr[stateIdx] === undefined) {
+      setState = (newState) => {
+        if (state === newState) {
+          return;
+        }
+        state = newState;
+        callback();
+      }
+      setStateArr.push(setState);
+    } else {
+      setState = setStateArr[stateIdx]
+    }
+    stateIdx++
+    return [state, setState]
+  }
+
 
   const useMemo = (fn, refs) => {
     return fn();
   };
 
   const resetContext = () => {
-
+    stateIdx = 0;
   }
 
   return { useState, useMemo, resetContext };
